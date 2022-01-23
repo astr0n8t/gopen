a8bc7594cc03b8ebd50d2c35e4f1371193c51e32
Nathan Higley <contact@nathanhigley.com>
2022-01-23 13:15:26 -0500

    Add initial file

---
+++ gopen.go
@@ -0,0 +1,29 @@
+/*
+----------------RecordKeeper----------------
+A dynamic DNS client written in Go with
+support for tracking the current public
+IP or a pre-configured address.
+------------Providers Supported-------------
+Cloudflare
+--------------------------------------------
+Written by Nathan Higley (@nadehi18)
+contact@nathanhigley.com
+https://nathanhigley.com
+https://github.com/nadehi18
+--------------------------------------------
+*/
+
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
+)
+
+func main() {
+
+
+}
